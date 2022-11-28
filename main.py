c78e2d21b7721cfb6980004d475065604e57d9de
Niv <niv.eckhaus@gmail.com>
2022-11-28 18:21:49 +0200

    added more checks like laughs and greetings

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ EMOJIS = [":)", ":(", "(:", "):", ":D", "D:",
 EMOJIS_FOR_REGEX = ["".join(["[" + char + "]" for char in emoji])
                     for emoji in EMOJIS]
 GREETINGS = ["hello", "hi", "hey"]
-GREETING_PATTERNS = ["\\b" + word + "[" + word[-1] + "]*\\b"
-                     for word in GREETINGS]
+GREETING_PATTERNS = ["\\b" + word + "+\\b" for word in GREETINGS]
+DIFFERENT_LENGTHS_HAHA = [rf"\ba*{'ha' * i}h*\b" for i in range(1, 21)]
+LAUGHS_PATTERN = "|".join(DIFFERENT_LENGTHS_HAHA) + r"|\blol+\b|\blmao+\b"
 DOUBLE_PUNCTUATION = ("[" + string.punctuation + "]") * 2
 TRIPLE_LETTERS = "|".join([char * 3 for char in string.ascii_lowercase])
+HMM_PATTERN="\\W*\\bhm+[.]*\\b\\W*"
+ONLY_HMM_PATTERN = "\\A" + HMM_PATTERN + "\\Z"
 
 
 def main():
@@ -21,19 +24,20 @@ def main():
     data.find_identical_sentences_with_different_case()
     data.find_sentences_with_all_upper_words()
     data.lower_all_sentences()
+    data.find_sentences_with_str_from_group("sentences_with_hmm.txt",
+                                            pattern=HMM_PATTERN)
+    data.find_sentences_with_str_from_group("only_hmm_sentences.txt",
+                                            pattern=ONLY_HMM_PATTERN)
+    data.find_sentences_with_str_from_group("sentences_with_laughs.txt",
+                                            pattern=LAUGHS_PATTERN)
     data.find_sentences_with_str_from_group("sentences_with_greeting.txt",
                                             group_of_strs=GREETING_PATTERNS,
                                             strs_are_words=False)
-    data.find_sentences_with_str_from_group("sentences_with_emojies.txt",
+    data.find_sentences_with_str_from_group("sentences_with_emojis.txt",
                                             group_of_strs=EMOJIS_FOR_REGEX,
                                             strs_are_words=False)
     data.find_sentences_with_str_from_group("sentences_with_double_punc.txt",
                                             pattern=DOUBLE_PUNCTUATION)
-    data.find_sentences_with_str_from_group("sentences_with_mm.txt",
-                                            pattern="mm")
-    data.find_sentences_with_str_from_group(pattern="[.][.]",
-                                            dest_path=
-                                            "sentences_with_2_dots.txt")
     data.find_sentences_with_str_from_group("sentences_with_triplets.txt",
                                             pattern=TRIPLE_LETTERS)
 

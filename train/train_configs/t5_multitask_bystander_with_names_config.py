5d4252fa621f2f274b64943ba4b195157c80f5da
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-01-22 10:39:32 +0200

    Moved the max source and target length flags to the default config

--- train/train_configs/t5_multitask_bystander_with_names_config.py
+++ train/train_configs/t5_multitask_bystander_with_names_config.py
@@ -1,6 +1,6 @@
 from train_configs.default_config import DefaultConfig
 
-class SummTrialConfig(DefaultConfig):
+class T5MultitaskBystanderWithNamesConfig(DefaultConfig):
     def __init__(self):
         super().__init__(
             train_file='bystanders_training_data_with_names',
@@ -10,8 +10,5 @@ class SummTrialConfig(DefaultConfig):
             num_train_epochs='5',
             per_device_train_batch_size='1',
             gradient_accumulation_steps='8',
-            max_source_length='2048',
-            max_target_length='64',
-            val_max_target_length='128',
             learning_rate='1e-4'
         )

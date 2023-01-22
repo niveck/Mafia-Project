816002759e2c1b187bae12d9efa1b1e0cc9e754c
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-01-22 11:27:44 +0200

    Added special tokens to the tokenizer

--- train/train.py
+++ train/train.py
@@ -407,6 +407,8 @@ def main():
         use_auth_token=True if model_args.use_auth_token else None,
     )
 
+    tokenizer.add_tokens(['<player name>', '<phase change>', '<vote>', '<victim>', '<text>'])
+
     # We resize the embeddings only when necessary to avoid index errors. If you are creating a model from scratch
     # on a small vocab and want a smaller embedding size, remove this test.
     embedding_size = model.get_input_embeddings().weight.shape[0]

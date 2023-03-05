6165ac8f7553557cc6aa14172db808682a18517a
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-03-05 10:15:34 +0200

    Added a new config

---
+++ train/train_configs/t5_multitask_bystander_with_names_and_votes_all_config.py
@@ -0,0 +1,15 @@
+from train_configs.default_config import DefaultConfig
+
+class T5MultitaskBystanderWithNamesAndVotesAllConfig(DefaultConfig):
+    def __init__(self):
+        super().__init__(
+            train_file='training_data/training_by_all_data_with_or_without_votes_february_2023/train_data_with_votes_with_names.csv',
+            validation_file='training_data/training_by_all_data_with_or_without_votes_february_2023/validation_data_with_votes_with_names.csv',
+            input_column='accumulated_messages',
+            target_column='current_turn_player_message',
+            num_train_epochs='5',
+            per_device_train_batch_size='1',
+            gradient_accumulation_steps='8',
+            learning_rate='1e-4',
+            output_dir='train_output_bystander_names_and_votes_all'
+        )

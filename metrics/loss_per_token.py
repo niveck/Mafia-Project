4d7b1fbc58e9742cf18a76e4be3661813febe522
Uri Berger <uriber@cs.huji.ac.il>
2023-03-22 20:24:26 +0200

    Fixed a bug in the loss by token script

--- metrics/loss_per_token.py
+++ metrics/loss_per_token.py
@@ -7,7 +7,7 @@ def extract_loss_per_token(model, tokenizer, text):
     while len(cur_ids_list) < all_input_ids.shape[1]:
         cur_input_ids = torch.tensor([cur_ids_list])
         next_token = all_input_ids[0, len(cur_ids_list)].item()
-        cur_labels = torch.tensor([next_token])
+        cur_labels = torch.tensor([[next_token]])
         outputs = model(input_ids=cur_input_ids, labels=cur_labels)
         loss = outputs.loss.item()
         loss_list.append(loss)

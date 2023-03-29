90e8a53c3c1bdcc0a2c14150c50c3dc463264bb6
Uri Berger <uriber@cs.huji.ac.il>
2023-03-29 14:31:47 +0300

    Fixed a typo

--- metrics/generate_outputs.py
+++ metrics/generate_outputs.py
@@ -1,6 +1,6 @@
 def generate_outputs(model, tokenizer, prefix, text):
     prefix_ids = tokenizer(prefix, return_tensors="pt").input_ids
-    text_ids = tokenizer(text, return_tensors="pt").input_ids[0, :]
+    text_ids = tokenizer(text, return_tensors="pt").input_ids
     outputs = model(input_ids=prefix_ids, labels=text_ids)
 
     return outputs

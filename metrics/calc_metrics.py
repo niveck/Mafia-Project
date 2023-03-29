36d5f4e66886ab7867e590ab19acde0d830ef499
Uri Berger <uri.berger2@mail.huji.ac.il>
2023-03-29 12:46:33 +0300

    added perplexity metric

---
+++ metrics/calc_metrics.py
@@ -0,0 +1,19 @@
+from metrics.generate_outputs import generate_outputs
+import torch
+
+def generate_outputs(model, tokenizer, prefix, text):
+    outputs = generate_outputs(model, tokenizer, prefix, text)
+
+    # Loss
+    loss = outputs.loss
+
+    # Perplexity
+    logits = outputs.logits[0, :, :]
+    probs = torch.softmax(logits, dim=1)
+    text_ids = tokenizer(text).input_ids[:-1]
+    seq_prob = 1
+    for cur_id in text_ids:
+        seq_prob *= probs[cur_id]
+    perplexity = (1/seq_prob)**(1/len(text_ids))
+
+    return loss, perplexity
--- metrics/loss_metric.py
+++
@@ -1,5 +0,0 @@
-from metrics.loss_per_token import extract_loss_per_token
-
-def loss_metric(model, tokenizer, prefix, text):
-    loss_list = extract_loss_per_token(model, tokenizer, prefix, text)
-    return sum(loss_list)/len(loss_list)

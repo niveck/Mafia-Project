c0d53ad704246e42f0feb562af216b78bdc947ec
Uri Berger <uriber@cs.huji.ac.il>
2023-04-20 11:33:58 +0300

    Fixed a typo

--- train/train_configs/bart_multitask_bystander_with_struct_data_with_names_all_config.py
+++ train/train_configs/bart_multitask_bystander_with_struct_data_with_names_all_config.py
@@ -4,7 +4,7 @@ class BartMultitaskBystanderWithStructDataWithNamesAllConfig(DefaultConfig):
     def __init__(self):
         super().__init__(
             train_file='training_data/training_by_all_data_with_structured_data_april_2023/train_data_with_structured_data_without_votes_with_names.csv',
-            validation_file='training_data/training_by_all_data_with_or_without_votes_february_2023/validation_data_with_structured_data_without_votes_with_names.csv',
+            validation_file='training_data/training_by_all_data_with_structured_data_april_2023/validation_data_with_structured_data_without_votes_with_names.csv',
             input_column='game_data_until_now',
             target_column='player_message',
             num_train_epochs='5',

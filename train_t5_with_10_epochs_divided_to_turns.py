be85197d0b7e56d58c3be06fae1c396e0c8d391e
niveck <niv.eckhaus@gmail.com>
2023-06-24 17:19:05 +0300

    changed config path in training script

--- train_t5_with_10_epochs_divided_to_turns.py
+++ train_t5_with_10_epochs_divided_to_turns.py
@@ -10,6 +10,5 @@ if __name__ == "__main__":
     decompress_file(r"./training_data/"
                     r"training_by_all_messages_without_votes_divided_to_turns_june_2023/"
                     r"validation_data.csv.gz")
-    run_train("train.train_configs."
-              "t5_multitask_bystander_with_names_without_votes_divided_to_turns_config."
+    run_train("t5_multitask_bystander_with_names_without_votes_divided_to_turns_config."
               "T5MultitaskBystanderWithNamesWithoutVotesDividedToTurnsConfig")

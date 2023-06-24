42abc2d441218614e5d92e1c932c84903d5881de
niveck <niv.eckhaus@gmail.com>
2023-06-24 17:15:37 +0300

    fixed files to have .gz

--- train_t5_with_10_epochs_divided_to_turns.py
+++ train_t5_with_10_epochs_divided_to_turns.py
@@ -4,12 +4,12 @@ from train.train_runner import run_train
 
 if __name__ == "__main__":
     prepare()
-    decompress_file(r"/cs/snapless/gabis/nive/Mafia-Project/training_data/"
+    decompress_file(r"./training_data/"
                     r"training_by_all_messages_without_votes_divided_to_turns_june_2023/"
-                    r"train_data.csv")
-    decompress_file(r"/cs/snapless/gabis/nive/Mafia-Project/training_data/"
+                    r"train_data.csv.gz")
+    decompress_file(r"./training_data/"
                     r"training_by_all_messages_without_votes_divided_to_turns_june_2023/"
-                    r"validation_data.csv")
+                    r"validation_data.csv.gz")
     run_train("train.train_configs."
               "t5_multitask_bystander_with_names_without_votes_divided_to_turns_config."
               "T5MultitaskBystanderWithNamesWithoutVotesDividedToTurnsConfig")

963e44de4b1498563258060d67539fdb7b38ea74
niveck <niv.eckhaus@gmail.com>
2023-06-24 17:59:29 +0300

    fixed target column in config

--- train/train_configs/t5_multitask_bystander_with_names_without_votes_divided_to_turns_config.py
+++ train/train_configs/t5_multitask_bystander_with_names_without_votes_divided_to_turns_config.py
@@ -7,7 +7,7 @@ class T5MultitaskBystanderWithNamesWithoutVotesDividedToTurnsConfig(DefaultConfi
             train_file='training_data/training_by_all_messages_without_votes_divided_to_turns_june_2023/train_data.csv',
             validation_file='training_data/training_by_all_messages_without_votes_divided_to_turns_june_2023/validation_data.csv',
             input_column='game_data_until_now',
-            target_column='current_turn_player_message',
+            target_column='player_message',
             num_train_epochs='10',
             per_device_train_batch_size='1',
             gradient_accumulation_steps='8',

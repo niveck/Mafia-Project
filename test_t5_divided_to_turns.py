82248dcb49a9b1179dc85fb83ea115ee1ab52628
niveck <niv.eckhaus@gmail.com>
2023-06-25 16:04:32 +0300

    rerun prediction for turn-division without first game

--- test_t5_divided_to_turns.py
+++ test_t5_divided_to_turns.py
@@ -8,9 +8,10 @@ OUTPUT_PATH_DIR = "./predictions/t5_large_10_epochs_divided_to_turns/"
 VALIDATION_SET_DIR = "./training_data/training_by_all_messages_without_votes_divided_to_turns_june_2023/"
 VALIDATION_SET_FILE = "validation_data.csv"
 VALIDATION_SET_PATH = VALIDATION_SET_DIR + VALIDATION_SET_FILE
-GAME_IDS = ["f0ac69d0-a9ee-662f-6cdd-b64dbf23a72d-data",
-            "f2c4a8cd-7cf5-efa0-bc21-b8f655da8b09-data",
-            "fb8339cf-c251-ac37-d742-4c4b3ee16a53-data"]
+GAME_IDS = [
+    # "f0ac69d0-a9ee-662f-6cdd-b64dbf23a72d-data",
+    "f2c4a8cd-7cf5-efa0-bc21-b8f655da8b09-data",
+    "fb8339cf-c251-ac37-d742-4c4b3ee16a53-data"]
 
 
 def main(game_id):

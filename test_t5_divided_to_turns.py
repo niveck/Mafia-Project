10fba25c3fdf7a5a6c8d868b571788c88b9d22dc
niveck <niv.eckhaus@gmail.com>
2023-06-25 12:12:27 +0300

    some more fixes

--- test_t5_divided_to_turns.py
+++ test_t5_divided_to_turns.py
@@ -4,16 +4,6 @@ from prepare import prepare
 
 MODEL_PATH = "train_output_divided_to_turns_10_epochs"
 MODEL_MAX_SOURCE_LENGTH = 512
-# EXAMPLE_MESSAGE = "<phase change> Nighttime <player name> Natalie Morris <text> Yo <player name> " \
-#                   "Ryan Hodges <text> hi <player name> Natalie Morris <text> who shall we knock " \
-#                   "off first? Bryce? <player name> Ryan Hodges <text> sure <player name> Ryan " \
-#                   "Hodges <vote> Bryce Fields <player name> Natalie Morris <vote> Bryce Fields " \
-#                   "<phase change> Daytime <phase change> Daytime <player name> Mary Trujillo " \
-#                   "<text> hello <player name> Christina Rollins <text> Hi <player name> Troy " \
-#                   "Thomas <text> I don't really understand how we're supposed to know who to " \
-#                   "vote for, I just chose Bryce Fields at random.. <player name> Ryan Hodges " \
-#                   "<text> hey <player name> Christopher Smith <text> Hello <player name> Diana " \
-#                   "Pennington <text> "
 OUTPUT_PATH_DIR = "./predictions/t5_large_not_fine_tuned/with_instruction/"
 VALIDATION_SET_DIR = "./training_data/training_by_all_messages_without_votes_divided_to_turns_june_2023/"
 VALIDATION_SET_FILE = "validation_data.csv"

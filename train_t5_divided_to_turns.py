e9a69c28eee60ad5ee783f21a4b417f9d60193c0
niveck <niv.eckhaus@gmail.com>
2023-07-18 16:35:43 +0300

    trying 5 messages to 1 pass ratio

--- train_t5_divided_to_turns.py
+++ train_t5_divided_to_turns.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     # decompress_file(r"./training_data/"
     #                 r"training_by_all_messages_without_votes_divided_to_turns_june_2023/"
     #                 r"validation_data.csv.gz")
-    run_train("t5_multitask_bystander_divided_to_turns_with_1_pass_to_20_config."
-              "T5MultitaskBystanderDividedToTurnsWith1PassTo20Config")
+    run_train("t5_multitask_bystander_divided_to_turns_with_1_pass_to_5_config."
+              "T5MultitaskBystanderDividedToTurnsWith1PassTo5Config")

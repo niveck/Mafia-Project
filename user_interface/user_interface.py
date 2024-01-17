7a980b86054ea49065d86dd83778a1792c3f175a
niveck <niv.eckhaus@gmail.com>
2024-01-17 13:36:33 +0200

    merged with changes on CS computer

--- user_interface/user_interface.py
+++ user_interface/user_interface.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit.components.v1 import html
 from streamlit_autorefresh import st_autorefresh
 from datetime import datetime
 import time
@@ -14,15 +15,17 @@ DAYTIME = 3
 GAME_ENDED = 4
 
 # Game hyperparameters:
-NUM_PLAYERS = 2
-NUM_MAFIA = 1
+PLAYERS = ["Bystander", "Mafioso", "Bystander", "Bystander",
+           "Bystander", "Mafioso", "Bystander", "Bystander"]
 
 # Game timers:
-ONE_SECOND = 100  # 100 milliseconds
+ONE_SECOND = 1000  # 1000 milliseconds
 INSTRUCTION_READING_TIME = 5
 
 # Game data files:
-PARTICIPANTS_RECORD = "./user_interface/participants_record.txt"
+PARENT_DIR = "/cs/snapless/gabis/nive/Mafia-Project/"
+PARTICIPANTS_RECORD = PARENT_DIR + "user_interface/participants_record.txt"
+OUTPUT_PATH = PARENT_DIR + "user_interface/game_output.csv"
 
 
 def new_player_entry_page():
@@ -50,10 +53,7 @@ def all_players_entered():
         st.session_state["players"] = []
     with open(PARTICIPANTS_RECORD, "r") as f:
         st.session_state["players"] = f.readlines()
-    if len(st.session_state["players"]) == NUM_PLAYERS:
-        return True
-    else:
-        return False
+    return len(st.session_state["players"]) == len(PLAYERS)
 
 
 def waiting_for_other_users_to_enter_page():
@@ -66,15 +66,18 @@ def waiting_for_other_users_to_enter_page():
 
 def timer_and_move_to_page(seconds, page):
     for _ in range(seconds):
-        time.sleep(1)
+        time.sleep(1)  # todo can it just be time.sleep(seconds?)
     move_to_page(page)
     st_autorefresh(interval=ONE_SECOND, key=f"timer_move_to_{page}")
 
 
 def instructions_page():
     st.title(f"**Welcome all players:**")
-    for player_name in st.session_state["players"]:
+    for player_index, player_name in enumerate(st.session_state["players"]):
         st.text(player_name)
+        if player_name.strip() == st.session_state["name"]:
+            st.session_state["role"] = PLAYERS[player_index]
+    st.info(f"Your role is {st.session_state['role']}")
     st.header("Game instructions:")
     st.text("- Write in English\n- Don't reveal your role")
     timer_and_move_to_page(INSTRUCTION_READING_TIME, NIGHTTIME)
@@ -82,6 +85,39 @@ def instructions_page():
 
 def nighttime_page():
     st.title("Nighttime")
+    st.info(f"Your role is {st.session_state['role']}")
+    my_html = """
+    <script>
+    function startTimer(duration, display) {
+        var timer = duration, minutes, seconds;
+        setInterval(function () {
+            minutes = parseInt(timer / 60, 10)
+            seconds = parseInt(timer % 60, 10);
+
+            minutes = minutes < 10 ? "0" + minutes : minutes;
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.textContent = minutes + ":" + seconds;
+
+            if (--timer < 0) {
+                timer = duration;
+            }
+        }, 1000);
+    }
+
+    window.onload = function () {
+        var turnSeconds = 90,
+            display = document.querySelector('#time');
+        startTimer(turnSeconds, display);
+    };
+    </script>
+
+    <body>
+      <div>Registration closes in <span id="time">01:30</span> minutes!</div>
+    </body>
+    """
+
+    html(my_html)
     # todo continue
 
 
--- user_interface/user_interface_draft.py
+++ user_interface/user_interface_draft.py
@@ -2,13 +2,12 @@ import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 from datetime import datetime
 import time
-OUTPUT_PATH = "/cs/snapless/gabis/nive/Mafia-Project/user_interface/game_output.csv" # todo validate relative path
+
+OUTPUT_PATH = "/cs/snapless/gabis/nive/Mafia-Project/user_interface/game_output.csv"
 OUTPUT_FILE_HEADER = "time,name,type,message"
 
 
-
 def main():
-
     if 'name' not in st.session_state:
         st.title("Welcome to the Messaging App")
 
@@ -17,13 +16,11 @@ def main():
             st.session_state["name_input"] = ""
 
         st.text_input("Enter your name", key="name_input", on_change=submit_name)
-
     else:
         messaging_page()
 
 
 def messaging_page():
-
     # todo maybe try https://stackoverflow.com/questions/74910140/how-do-i-update-values-in-streamlit-on-a-schedule
     # todo or maybe https://stackoverflow.com/questions/62718133/how-to-make-streamlit-reloads-every-5-seconds
     # todo or read more https://docs.streamlit.io/library/advanced-features/caching
@@ -35,7 +32,14 @@ def messaging_page():
     name = st.session_state["name"]
 
     with open(OUTPUT_PATH, "r") as f:
-        st.session_state["messages"] = f.readlines()
+        lines = f.readlines()
+
+    if len(lines) > 1 and lines[-1] == lines[-2]:
+        lines.pop()
+        with open(OUTPUT_PATH, "w") as f:
+            f.writelines(lines)
+
+    st.session_state["messages"] = lines
 
     st.text("All messages:")
     for message in st.session_state["messages"]:
@@ -48,13 +52,13 @@ def messaging_page():
         line = f"{now},{name},text,{st.session_state['user_text_input']}"
         st.session_state["user_text_input"] = ""
         with open(OUTPUT_PATH, "a") as f:
-            f.write(line + "\n")
+            if "messages" in st.session_state and len(st.session_state["messages"]) > 0 \
+                    and line != st.session_state["messages"][-1]:
+                f.write(line + "\n")
 
     st.text_input("Write a message", key="user_text_input", on_change=submit_message)
-    time.sleep(4)
+    # time.sleep(4)
 
 
 if __name__ == "__main__":
     main()
-
-

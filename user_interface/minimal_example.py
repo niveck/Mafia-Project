db87e1586ec6c68c86816f1bdcd944667e1833ff
niveck <niv.eckhaus@gmail.com>
2024-01-24 15:41:10 +0200

    minimized the minimal

--- user_interface/minimal_example.py
+++ user_interface/minimal_example.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import asyncio
-
-
-PAGES_DURATION = {"NIGHTTIME": 5, "DAYTIME": 5}
-
-NEXT_PAGES = {"NIGHTTIME": "DAYTIME", "DAYTIME": "NIGHTTIME"}
 
 
 def move_to_page(page):
@@ -12,7 +6,7 @@ def move_to_page(page):
 
 
 def new_player_entry_page():
-    st.title("Welcome to the game of Mafia!")
+    st.title("Welcome to a minimal example...")
     def enter_waiting_room():  move_to_page("WAITING_FOR_OTHER_USERS_TO_ENTER")
     st.button("Click to move to waiting room", key="enter_waiting_room",
               on_click=enter_waiting_room)
@@ -24,20 +18,9 @@ def waiting_for_other_users_to_enter_page():
     move_to_page("NIGHTTIME")
 
 
-async def wait_timer_and_move_to_next_page(current_page):
-    await asyncio.sleep(PAGES_DURATION[current_page])
-    move_to_page(NEXT_PAGES[current_page])
-
-
 def nighttime_page():
     st.title("Nighttime")
     st.info("Your role is bystander")
-    asyncio.run(wait_timer_and_move_to_next_page("NIGHTTIME"))
-
-
-def daytime_page():
-    st.title("**You've reached Daytime!**")
-    asyncio.run(wait_timer_and_move_to_next_page("DAYTIME"))
 
 
 def main():
@@ -50,8 +33,6 @@ def main():
         waiting_for_other_users_to_enter_page()
     elif page == "NIGHTTIME":
         nighttime_page()
-    elif page == "DAYTIME":
-        daytime_page()
 
 
 if __name__ == "__main__":

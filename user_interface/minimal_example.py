b2aaa1eaa0f6e509fbff255db684d1b25cd7d786
niveck <niv.eckhaus@gmail.com>
2024-01-24 16:17:47 +0200

    added asyncio to minimal, but doesnt work

--- user_interface/minimal_example.py
+++ user_interface/minimal_example.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import asyncio
+from streamlit_autorefresh import st_autorefresh
+
+
+PAGES_DURATION = {"NIGHTTIME": 5, "DAYTIME": 5}
+NEXT_PAGES = {"NIGHTTIME": "DAYTIME", "DAYTIME": "NIGHTTIME"}
 
 
 def move_to_page(page):
@@ -6,7 +12,7 @@ def move_to_page(page):
 
 
 def new_player_entry_page():
-    st.title("Welcome to a minimal example...")
+    st.title("Welcome to the game of Mafia!")
     def enter_waiting_room():  move_to_page("WAITING_FOR_OTHER_USERS_TO_ENTER")
     st.button("Click to move to waiting room", key="enter_waiting_room",
               on_click=enter_waiting_room)
@@ -15,12 +21,25 @@ def new_player_entry_page():
 def waiting_for_other_users_to_enter_page():
     st.title(f"**Welcome!**")
     st.info("Waiting for other players to enter...")
+    st_autorefresh(interval=1000, key="waiting_for_players")
     move_to_page("NIGHTTIME")
+
+
+async def wait_timer_and_move_to_next_page(current_page):
+    st_autorefresh(interval=1000, key="waiting_for_players")
+    await asyncio.sleep(PAGES_DURATION[current_page])
+    move_to_page(NEXT_PAGES[current_page])
 
 
 def nighttime_page():
     st.title("Nighttime")
     st.info("Your role is bystander")
+    asyncio.run(wait_timer_and_move_to_next_page("NIGHTTIME"))
+
+
+def daytime_page():
+    st.title("**You've reached Daytime!**")
+    asyncio.run(wait_timer_and_move_to_next_page("DAYTIME"))
 
 
 def main():
@@ -33,6 +52,8 @@ def main():
         waiting_for_other_users_to_enter_page()
     elif page == "NIGHTTIME":
         nighttime_page()
+    elif page == "DAYTIME":
+        daytime_page()
 
 
 if __name__ == "__main__":
